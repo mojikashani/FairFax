35deaccf98b6e6226f071a71b39e4fd78664df22
Moji Kashani <mojtaba@moneybrilliant.com.au>
2018-03-02 17:55:40 +1100

    exchanging all current tests to local test

--- FairFaxTest/app/src/test/java/com/moji/fairfaxtest/PresentersLocalTest.kt
+++ FairFaxTest/app/src/test/java/com/moji/fairfaxtest/PresentersLocalTest.kt
@@ -14,6 +14,7 @@ import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
 import io.reactivex.schedulers.Schedulers
+import org.junit.Before
 import org.mockito.junit.MockitoJUnitRunner
 
 
@@ -26,16 +27,17 @@ class PresentersLocalTest {
     @Mock
     private val mMockContext: Context = mock(Context::class.java)
 
-
-    // test if NewsPresenter return the api response on onNewsFetched
-    @Test
-    fun presenter_returns_at_least_one_new_asset() {
-        //setup
+    @Before
+    fun setUp() {
         val connectivityManager: ConnectivityManager = mock(ConnectivityManager::class.java)
         val networkInfo: NetworkInfo = mock(NetworkInfo::class.java)
         Mockito.`when`( mMockContext.getSystemService(Context.CONNECTIVITY_SERVICE)).thenReturn( connectivityManager )
         Mockito.`when`( connectivityManager.activeNetworkInfo).thenReturn( networkInfo )
-         // exercise
+    }
+
+    // test if NewsPresenter return the api response on onNewsFetched
+    @Test
+    fun presenter_returns_at_least_one_new_asset() {
         val presenter = NewsPresenter(mMockContext)
         presenter.attachListener( object : NewsListener {
             override fun onNewsFetched(newsAssets: List<NewsAssetView>) {
@@ -54,6 +56,68 @@ class PresentersLocalTest {
 
             override fun onAuthorizationError(e: Throwable) {
                 fail()
+            }
+
+            override fun showProgress(message: String) {
+            }
+        })
+        presenter.getNewsList(Schedulers.newThread())
+    }
+
+    // test if NewsPresenter calls showProgress to notify UI
+    // during the api request
+    @Test
+    fun presenter_notify_to_show_progress_view() {
+        val presenter = NewsPresenter(mMockContext)
+        var isShowProgressCalled = false
+        presenter.attachListener( object : NewsListener{
+            override fun onNewsFetched(newsAssets: List<NewsAssetView>) {
+                if(!isShowProgressCalled) {
+                    fail()
+                }
+            }
+            override fun hideProgress() {
+            }
+
+            override fun onError(message: String?) {
+            }
+
+            override fun onNoNetworkError() {
+            }
+
+            override fun onAuthorizationError(e: Throwable) {
+            }
+
+            override fun showProgress(message: String) {
+                isShowProgressCalled = true
+            }
+        })
+        presenter.getNewsList(Schedulers.newThread())
+    }
+
+    // test if NewsPresenter calls hideProgress to notify UI
+    // before calling onNewsFetch
+    @Test
+    fun presenter_notify_to_hide_progress_view() {
+        val presenter = NewsPresenter(mMockContext)
+        var isHideProgressCalled = false
+        presenter.attachListener( object : NewsListener{
+            override fun onNewsFetched(newsAssets: List<NewsAssetView>) {
+                if(!isHideProgressCalled){
+                    fail()
+                }
+            }
+            override fun hideProgress() {
+                isHideProgressCalled = true
+            }
+
+            override fun onError(message: String?) {
+            }
+
+            override fun onNoNetworkError() {
+            }
+
+            override fun onAuthorizationError(e: Throwable) {
             }
 
             override fun showProgress(message: String) {
